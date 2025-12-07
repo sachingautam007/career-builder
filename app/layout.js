1ad116e7e10d74d04d97b48de9e56d0bdd0b2489
sachingautam007 <sachingautam865412@gmail.com>
2025-12-07 13:55:19 +0530

    supreessHyradtionWarning

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
