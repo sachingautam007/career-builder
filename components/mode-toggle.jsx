7ebfc7546390adc140df68382fee14eefff5b410
sachingautam007 <sachingautam865412@gmail.com>
2025-12-20 22:46:10 +0530

    changing layout

---
+++ components/mode-toggle.jsx
@@ -0,0 +1,37 @@
+"use client"
+
+import * as React from "react"
+
+import { Moon, Sun } from "lucide-react"
+
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="dark:hidden" />
+          <Moon className="hidden dark:inline" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
